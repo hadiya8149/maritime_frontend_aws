806f79e4d0e69e8485d8b978ffd3c15b0c39d5ea
hadiya8149 <hadiya8149@gmail.com>
2024-06-08 10:55:35 +0500

    adding backend

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Footer from './components/footer';
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
