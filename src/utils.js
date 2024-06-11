5b3a055dd6fe7525623e532ac273a671a930d26b
hadiya8149 <hadiya8149@gmail.com>
2024-06-11 16:02:03 +0500

    authorization|

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://mbuig2i6bdtonzsxfxbuohmvxq0esskf.lambda-url.ap-southeast-2.on.aws/api'
-export const API_URL = 'http://localhost:8000/api'
+export const API_URL = 'https://mbuig2i6bdtonzsxfxbuohmvxq0esskf.lambda-url.ap-southeast-2.on.aws/api'
+// export const API_URL = 'http://localhost:8000/a/pi'
